2c6a74171d93918052fc0015399503aca56a44c8
f14shm4n <uraborg@gmail.com>
2020-09-20 20:12:11 +0300

    Added extension method to configure XForwarded headers.

--- f14.AspNetCore.Mvc/IServiceCollectionExtensions.cs
+++ f14.AspNetCore.Mvc/IServiceCollectionExtensions.cs
@@ -1,5 +1,8 @@
 ﻿using f14.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace f14.AspNetCore.Mvc
 {
@@ -14,6 +17,26 @@ namespace f14.AspNetCore.Mvc
         public static IServiceCollection AddViewRenderer(this IServiceCollection services)
         {
             services.AddScoped<IViewRenderer, ViewRenderer>();
+            return services;
+        }
+
+        /// <summary>
+        /// Configures the <see cref="ForwardedHeadersOptions"/> to use forwarded headers: <see cref="ForwardedHeaders.XForwardedFor"/> and <see cref="ForwardedHeaders.XForwardedProto"/>.
+        /// </summary>
+        public static IServiceCollection ConfigureHttpForwarderHeaders(this IServiceCollection services)
+        {
+            if (string.Equals(Environment.GetEnvironmentVariable("ASPNETCORE_FORWARDEDHEADERS_ENABLED"), "true", StringComparison.OrdinalIgnoreCase))
+            {
+                services.Configure<ForwardedHeadersOptions>(options =>
+                {
+                    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+                    // Only loopback proxies are allowed by default.
+                    // Clear that restriction because forwarders are enabled by explicit 
+                    // configuration.
+                    options.KnownNetworks.Clear();
+                    options.KnownProxies.Clear();
+                });
+            }
             return services;
         }
     }
