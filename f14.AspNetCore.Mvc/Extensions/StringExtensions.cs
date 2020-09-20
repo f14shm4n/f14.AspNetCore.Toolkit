66741b79f23d05cd8456ba84a82b9686414ed602
f14shm4n <uraborg@gmail.com>
2020-09-20 20:11:39 +0300

    Added string extension that extracts contoller name.

---
+++ f14.AspNetCore.Mvc/Extensions/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace f14.AspNetCore.Mvc.Extensions
+{
+    /// <summary>
+    /// Provides an extensions methods for <see cref="string"/> type.
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Extracts the Asp.Net Core controller name from current string value. This method removes the string entry 'Controller' from source string.
+        /// </summary>
+        /// <param name="source">The source value as controller name.</param>
+        /// <returns>The string value which contains substring of source string without 'Controller' token.</returns>
+        public static string GetControllerName(this string source) => source.Replace("Controller", "", StringComparison.OrdinalIgnoreCase);
+    }
+}
